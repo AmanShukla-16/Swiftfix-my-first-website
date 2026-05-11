eff95453d35e1fd9c076d46501c7d64274b54aa0
Aman Shukla <amanmshukla16@gmail.com>
2026-05-11 21:48:54 +0530

    Update workers.php

--- workers.php
+++ workers.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost","root","","service");
+$conn = new mysqli("localhost", "your_username", "your_password", "service");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
